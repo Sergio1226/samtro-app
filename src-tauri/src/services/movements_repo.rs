8c6d496bc5e76483efc4aabde0fa0a44469914e7
yaider <yaideralejo2@gmail.com>
2025-11-14 09:11:54 -0500

    Arreglado error tipografico

--- src-tauri/src/services/movements_repo.rs
+++ src-tauri/src/services/movements_repo.rs
@@ -4,7 +4,7 @@ use super::db;
 
 pub fn add_move(mov: &Move) -> rusqlite::Result<()> {
     let conn = db::get_connection()?;
-    conn.execute("INSERT INTO movements (product_code, stock, date, type_move) VALUES (?1, ?2, ?3, '4)", params![mov.product_code, mov.stock, mov.date, mov.type_move])?;
+    conn.execute("INSERT INTO movements (product_code, stock, date, type_move) VALUES (?1, ?2, ?3, ?4)", params![mov.product_code, mov.stock, mov.date, mov.type_move])?;
     
     Ok(())
 }
